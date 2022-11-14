7f8c092772775df1145b1ebc13a86d467b740a55
Chema Torres <chemaaa94@gmail.com>
2022-11-14 10:27:50 -0600

    poke api avances

--- src/app/components/poke-table/poke-table.component.ts
+++ src/app/components/poke-table/poke-table.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-poke-table',
@@ -7,9 +10,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokeTableComponent implements OnInit {
 
-  constructor() { }
+  displayColumns: string[] = ['position', 'image', 'name'];
+  data: any[] = [];
+  datasource = new MatTableDataSource<any>(this.data);
+  pokemons =  [];
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  constructor(private pokeService: PokemonService) { }
 
   ngOnInit(): void {
+    this.getPokemons();
   }
+
+  getPokemons(){
+    let pokemonData;
+
+    for(let i = 1; i<= 151; i++){
+      this.pokeService.getPokemons(i).subscribe(
+
+        res =>{
+          pokemonData = {
+            position: i,
+            image: res.sprites.front_default,
+            name: res.name
+          };
+          this.data.push(pokemonData);
+          this.datasource = new MatTableDataSource<any>(this.data)
+          this.datasource.paginator  = this.paginator
+        },
+        err =>{
+          console.log(err);
+
+        }
+      )
+    }
+  }
+
+    applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.datasource.filter = filterValue.trim().toLowerCase();
+
+    if (this.datasource.paginator) {
+      this.datasource.paginator.firstPage();
+    }
+  }
+
+
+getRow(row){
+  console.log(row);
+
+}
 
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  baseUrl = environment.baseUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getPokemons(index){
+    return this.http.get<any>(`${this.baseUrl}/pokemon/${index}`)
+  }
 }
